b9d91266d1043026dd140d421160b6974e7a6d84
Nathan Bertram <nbertram@gmail.com>
2012-04-10 13:10:56 -0600

    Scheduled April's meetup

--- winnipeg.rb
+++ winnipeg.rb
@@ -36,7 +36,7 @@ end
 
 get '/events' do 
   @page_title = 'Upcoming Events'
-  @events = %w(october_2011 september_2011 august_2011 june_2011 may_2011 april_2011 march_2011 february_2011 janurary_2011 november_2010 october_2010 september_2010 august_2010 july_2010)
+  @events = %w(february_2012 october_2011 september_2011 august_2011 june_2011 may_2011 april_2011 march_2011 february_2011 janurary_2011 november_2010 october_2010 september_2010 august_2010 july_2010)
   haml :events
 end
 
