e4b93aed329d1d3b0e8f766cdb3c7357e6f0a3fe
Sean Walberg <sean@ertw.com>
2015-04-22 19:28:16 -0500

    Read the meetup API for the next meeting.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'haml'
+
+gem 'awesome_print'
+gem 'rMeetup', git: 'git@github.com:winnipegrb/rmeetup.git'
--- app.rb
+++ app.rb
@@ -1,5 +1,34 @@
 require 'sinatra'
+require 'rmeetup'
+require 'awesome_print'
 
 get '/' do
+  @result = UpcomingMeeting.new
   haml :index
+end
+
+class UpcomingMeeting
+  extend Forwardable
+  attr_reader :next_meeting
+
+  delegate [:name, :description, :event_url] => :next_meeting
+  def initialize
+    RMeetup::Client.api_key = ENV['MEETUP_KEY']
+    @next_meeting = RMeetup::Client.fetch(:events,{group_urlname: "winnipegrb"}).first
+  end
+
+  def venue
+    "%s<br>%s" % [ vinfo('name'), vinfo('address_1') ]
+  end
+
+  def time
+    @next_meeting.time.strftime "%a, %b %d, %Y"
+  end
+
+  private
+
+  def vinfo(element)
+    @next_meeting.venue[element]
+  end
+
 end
